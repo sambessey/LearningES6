8fb2ce51c29b053fa2587fcb5433dfe9cb9a8dbb
Sam Bessey <sam@Sams-MBP.lan>
2018-12-28 12:30:12 +0000

    Basics working

---
+++ characters.js
@@ -0,0 +1,8 @@
+exports.attributes = () => {
+  let names = ["sammy","james","vicky","cat","Kris"];
+  let spells = ["shrinking","invsibility","fireballs","freeze"];
+  const attributes = [names,spells];
+  return(attributes)
+//  return(names, spells);
+}
+//module.exports=characters()
--- this-callbacks.js
+++ this-callbacks.js
@@ -1,18 +1,35 @@
+var characters = require ('./characters.js');
 class Hero {
 
     constructor(name, level) {
-        this.name = name;
         this.level = level;
+        this._name = randomise(name);
+        //This is a backing underscore that stops an infinite loop occurring when
+        //we try and get hold of the name property.
     }
+
+  set name(newName){
+   this._name = name;
+   console.log("HEY!!!!!!!");
+  }
+
+ get name() {
+   return this._name.toUpperCase();
+  }
+  logthings(name){
+  console.log("NAMES ARE!!! "+name);
+  }
 }
 
 class Mage extends Hero {
 
     constructor(name, level, spell) {
       super(name, level);
-      this.spell = spell;
+      this.spell = randomise(spell);;
+
     }
-    logstuff(){console.log("hey")}
+
+    logstuff(stuff){console.log("accessed"+stuff)}
     //Regular methods are set against the instance of the object (i.e. the hero)
 }
 
@@ -21,6 +38,8 @@ class MagicalMage extends Mage {
     constructor(name, level, spell, potency) {
       super(name, level, spell);
       this.potency = potency;
+      //return super.logstuff(" the child class");
+      return super.logthings(name);
     }
     static writeBack(){console.log("bleh");}
     //static methods require you to hit the CLASS (MagicalMage) as they are defined at _proto_
@@ -28,17 +47,18 @@ class MagicalMage extends Mage {
 
 var somethingelse = err => {console.log("Carrying on"+err);return;}
 var randomLevel = (max,min) => {return(Math.round(Math.random() * (max - min) + min))}
+var randomise = input => {return(input[Math.floor(Math.random() * input.length)])}
 
 const promiseBeforeMakingCharacters = () => new Promise((resolve, reject) => {
   let somenumber = Math.random();
   if (somenumber<1){resolve(somenumber);}
   throw new Error("One DOES equal one, dummy!");
   });
-//hi
 
 let mainCharacter;
-let startingLevelRange = [1,4]
-let heroics = ["sammy", randomLevel(...startingLevelRange), "Shrinking people"]
+let startingLevelRange = [1,4];
+let [name,spell] = characters.attributes();
+let heroics = [name, randomLevel(...startingLevelRange), spell]
 let spellstatus = "but he can't use it yet.";
 
 promiseBeforeMakingCharacters()
@@ -53,11 +73,12 @@ promiseBeforeMakingCharacters()
       spellstatus = `with a potency of ${mainCharacter.potency}`;
     }
   })
-//hi
   .then(() => {
   console.log(`our hero is ${mainCharacter.name}. He has a spell of ${mainCharacter.spell} ${spellstatus}`)
 //  MagicalMage.writeBack();
 //  mainCharacter.logstuff();
+let characters;
+
 })
 
 .catch(err => somethingelse(err))
